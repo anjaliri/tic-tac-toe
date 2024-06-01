f4bca361fb50ceacf64548f443d91b8e8bc4d5dd
anjaliri <anjalisikanya@gmail.com>
2024-06-01 16:13:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTocToe from './Comonenets/TicTocToe/TicTocToe';
+
+function App() {
+  return (
+    <div>
+      <TicTocToe/>
+    </div>
+  );
+}
+
+export default App;
